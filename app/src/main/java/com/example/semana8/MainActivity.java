4a76efbf339304184d941ce932eafa429cb02f59
NeoBurste <raul.aguirre.rojas2000@gmail.com>
2023-10-30 17:36:40 -0300

    Initial commit

---
+++ app/src/main/java/com/example/semana8/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.semana8;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnDescargar;
+    ImageView imagen;
+    Button sensores;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnDescargar = findViewById(R.id.descargarImagen);
+        imagen = findViewById(R.id.imagen);
+        sensores = findViewById(R.id.sensorRotacion);
+
+        sensores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SensorRotacion.class);
+                startActivity(intent);
+            }
+        });
+
+        btnDescargar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        final Bitmap bitmap = loadImageFromNetwork("https://www.asisbiz.com/il2/Ju-87/StG2/images/Junkers-Ju-87D-Stuka-II.StG2-coded-O-eastern-front-01.jpg");
+                        imagen.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                imagen.setImageBitmap(bitmap);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+    }
+
+    private Bitmap loadImageFromNetwork (String imageURL){
+        try {
+            URL url = new URL(imageURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream inputStream = connection.getInputStream();
+            return BitmapFactory.decodeStream(inputStream);
+
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/semana8/SensorRotacion.java
@@ -0,0 +1,78 @@
+package com.example.semana8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+
+public class SensorRotacion extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private Sensor rotationSensor;
+    private float[] rotationMatrix = new float[9];
+    private float[] orientationValues = new float[3];
+    private ImageView pelota;
+    private float currentX = 0; // Posición X de la pelota
+    private float currentY = 0; // Posición Y de la pelota
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_rotacion);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        rotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        pelota = findViewById(R.id.pelotaRoja);
+
+        if (rotationSensor != null) {
+            sensorManager.registerListener(sensorEventListener, rotationSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            // Handle the case where the rotation sensor is not available on the device.
+        }
+    }
+
+    private final SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+                SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values);
+                SensorManager.getOrientation(rotationMatrix, orientationValues);
+
+                float rotationX = orientationValues[0];
+                float rotationY = orientationValues[1];
+
+                // Actualizar la posición de la pelota en función de la rotación
+                updateBallPosition(rotationX, rotationY);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // Handle accuracy changes if necessary.
+        }
+    };
+
+    private void updateBallPosition(float rotationX, float rotationY) {
+        float speed = 5;
+
+
+        currentX += rotationX * speed;
+        currentY += rotationY * speed;
+
+        float maxX = getWindowManager().getDefaultDisplay().getWidth() - pelota.getWidth();
+        float maxY = getWindowManager().getDefaultDisplay().getHeight() - pelota.getHeight();
+        currentX = Math.max(0, Math.min(currentX, maxX));
+        currentY = Math.max(0, Math.min(currentY, maxY));
+
+        pelota.setX(currentX);
+        pelota.setY(currentY);
+    }
+}
